7045c947f034c8b89e8ed682e411db37ed1c5ef7
Scarrus666 <scarrus@gmail.com>
2023-07-12 15:08:08 +0200

    some changes

---
+++ header.php
@@ -0,0 +1,22 @@
+<div class="header">
+    <div class="navbar">
+      <a href="#" class="active">Home</a>
+      <a href="#">Articles</a>
+
+    </div>
+    <!-- <h1 class="header-title">Nutritional Advice</h1> -->
+
+    <img src="./images/logo_n.jpg" alt="Logo">
+
+    <div class="navbar">
+        <a href="#">Recipes</a>
+        <a href="#">About</a>
+    </div>
+</div>
+
+
+
+
+
+
+
---
+++ pageframe.php
@@ -0,0 +1,9 @@
+<div class="container">
+    <!-- Rest of the website content goes here -->
+
+    <h1>I test you</h1>
+
+    <p><br><br><br><br></p>
+    <p>This is some text</p>
+    <p><br><br><br><br></p>
+</div>
