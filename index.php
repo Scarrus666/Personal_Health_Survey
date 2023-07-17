dc7d625e31e574d1824782153763ad1a3f381094
“Scarrus666” <scarrus@gmail.com>
2023-07-17 16:54:02 +0200

    last commit

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
         include './template-range-slider-draft.php';
     ?> 
 
-    <h5>Frage <?php echo $questionIndex + 1; ?></h5>
+    <h5>Frage <?php echo "$questionIndex + 1"; ?></h5>
     <h3><?php echo $data["question-text"]; ?></h3>
     <form action="question-2.php" method="post" onsubmit="return validateRange('range-slider');">
         <p class="instruction"><?php echo $data["instruction"]; ?></p>
